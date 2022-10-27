04906085d6a7fc4b81d8b55ad890112fcfe81858
ayockson2500 <ayockson2500@gmail.com>
2022-10-27 13:23:16 +0100

    a theme index for styled component

---
+++ src/theme/index.js
@@ -0,0 +1,20 @@
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+    color: {
+        mainRedColor: "#EB1616",
+        mainLightRedColor: "#82191D",
+        mainDarkRedColor: "#4E1B21",
+        mainHoverRed: "#BC1212",
+        mainNavyColor: "#416593",
+        mainLightColor: "#FFFFFF",
+        primaryDarkBg: "#191C24",
+        secondaryDarkbg: "#000000",
+    }
+};
+
+const StyledThemeProvider =({children}) => {
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
+
+export default StyledThemeProvider;
