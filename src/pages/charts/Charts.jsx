bc82bcf04ca9fa6de2f70d0229e7f1068a7517c0
ayockson2500 <ayockson2500@gmail.com>
2022-10-28 11:12:25 +0100

    imported a barChart to the chart component

---
+++ src/pages/charts/Charts.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import BarChart from '../../components/charts/barChart/BarChart'
+import Layout from '../../layout'
+
+const Charts = () => {
+  return (
+    <div>
+        <Layout>
+          {/* <BarChart /> */}
+        </Layout>
+    </div>
+  )
+}
+
+export default Charts
