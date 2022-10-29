1929c442b67689a321660d98c2d7310702b59d05
ayockson2500 <ayockson2500@gmail.com>
2022-10-29 03:36:32 +0100

    bar chart component

--- src/components/charts/barChart/BarChart.jsx
+++ src/components/charts/barChart/BarChart.jsx
@@ -1,32 +1,57 @@
+import { Link } from "react-router-dom";
+import { Bar } from "react-chartjs-2";
+import { registerables, Chart } from "chart.js";
+import { StyledBarChart } from "./StyledBarChart.styles";
+Chart.register(...registerables);
 
-import { Bar } from "react-chartjs-2"
-import {CategoryScale} from 'chart.js'; 
-// Chart.register(CategoryScale);
-import { StyledBarChart } from "./StyledBarChart.styles"
+const BarChart = ({ className }) => {
+  //   const data =
 
-const BarChart = () => {
-      
-    //   const data = 
-     
-    
   return (
     <StyledBarChart>
+      <div className="d-flex align-items-center justify-content-between title-wrapper">
+            <p>Worldwide Sales</p>
+            <Link></Link>
+        </div>
+      <div>
         <Bar
-        height={500}
-        width={500}
-        data={{
+          className="chart"
+          data={{
             labels: [2016, 2017, 2018, 2019, 2020, 2021, 2022],
-            datasets: [{
+            datasets: [
+              {
                 label: "USA",
-                data: [0, 20, 40, 60, 80, 100],
-                backgroundColor: '#A9080B'
-            }]
-        }}
-        options={{maintainAspectRatio: false}}
+                data: [5, 10, 20, 40, 60, 80, 100],
+                backgroundColor: "#A9080B",
+              },
+              {
+                label: "UK",
+                data: [15, 30, 25, 50, 75, 83, 97],
+                backgroundColor: "#800E12",
+              },
+              {
+                label: "UK",
+                data: [8, 40, 30, 50, 90, 58, 60],
+                backgroundColor: "#571419",
+              },
+            ],
+          }}
+          height={250}
+          width={500}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              xAxes: {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            },
+          }}
         />
+      </div>
     </StyledBarChart>
-  )
-}
+  );
+};
 
-export default BarChart
-
+export default BarChart;
