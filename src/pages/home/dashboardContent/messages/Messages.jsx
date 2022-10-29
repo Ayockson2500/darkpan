4dd114e9aed33f62925f9ca11d730a02c7693dd4
ayockson2500 <ayockson2500@gmail.com>
2022-10-29 03:40:09 +0100

    messages component

---
+++ src/pages/home/dashboardContent/messages/Messages.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { StyledMessages } from "./StyledMessages.styles";
+import { Link } from "react-router-dom";
+import { messagesElement } from "./messagesElement";
+
+const Messages = () => {
+  return (
+    <StyledMessages>
+      <div className="d-flex align-items-center justify-content-between title-wrapper">
+        <p>Messages</p>
+        <Link>Show All</Link>
+      </div>
+      <div>
+        {messagesElement.map((content, index) => (
+          <div key={index} className="d-flex align-items-center contentItem">
+            <div>
+              <img
+                src={content.senderPhoto}
+                alt={content.senderName}
+                width="40"
+                height="40"
+                className="userImage"
+              />
+            </div>
+            <div>
+              <div className="d-flex align-items-center justify-content-between">
+                <p className="sender-name lightColor">{content.senderName}</p>
+                <p className="time GrayColor fs-14">{content.time}</p>
+              </div>
+              <p className="message GrayColor fs-14">{content.message}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </StyledMessages>
+  );
+};
+
+export default Messages;
---
+++ src/pages/home/dashboardContent/messages/messagesElement.js
@@ -0,0 +1,28 @@
+import userImage from "../../../../assets/images/ayock.jpg";
+
+export const messagesElement = [
+  {
+    senderName: "Jhon Doe",
+    message: "Short message goes here...",
+    time: "15 hr ago",
+    senderPhoto: userImage
+  },
+  {
+    senderName: "Jhon Doe",
+    message: "Short message goes here...",
+    time: "15 min ago",
+    senderPhoto: userImage
+  },
+  {
+    senderName: "Jhon Doe",
+    message: "Short message goes here...",
+    time: "15 min ago",
+    senderPhoto: userImage
+  },
+  {
+    senderName: "Jhon Doe",
+    message: "Short message goes here...",
+    time: "15 min ago",
+    senderPhoto: userImage
+  }
+]
