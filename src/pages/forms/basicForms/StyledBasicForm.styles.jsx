a8de6ad41633211ccb5ff0721ad957f161b92d39
ayockson2500 <ayockson2500@gmail.com>
2022-10-31 21:14:40 +0100

    add styles

--- src/pages/forms/basicForms/StyledBasicForm.styles.jsx
+++ src/pages/forms/basicForms/StyledBasicForm.styles.jsx
@@ -10,4 +10,42 @@ export const StyledBasicForm = styled.form`
         padding: 20px;
         border-radius: 5px;
         width: 100%;
+        .title {
+            color: ${props => props.theme.color.mainLightColor};
+        }
+        label {
+            color: ${props => props.theme.color.mainNavyColor};
+            font-size: 14px;
+        }
+        .inputField {
+            margin-bottom: 0;
+            padding: 4px;
+        }
+        .email-field-wrapper  {
+            margin-bottom: 10px;
+        }
+        .email-field-wrapper span {
+            color: #67674F;
+            font-size: 12px;
+        }
+        .checkbox-wrapper {
+            margin-bottom: 15px;
+        }
+        .checkbox-wrapper span {
+            color: ${props => props.theme.color.mainNavyColor};
+            font-size: 14px;
+            margin-left: 7px;
+        }
+        Button a {
+            padding: 4px 8px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        Button a {
+            text-decoration: none;
+            color: white;
+            font-weight: normal;
+            font-size: 14px;
+        }
 `
