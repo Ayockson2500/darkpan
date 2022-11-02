2bf7ed0fb2ff65f5f710fabcb763bdf898b5bc91
ayockson2500 <ayockson2500@gmail.com>
2022-11-02 08:08:21 +0100

    remove styles from styled component

--- src/pages/widgets/StyledWidget.styles.jsx
+++ src/pages/widgets/StyledWidget.styles.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const StyledWidget = styled.div`
     * {
         margin: 0;
-        padding: 0;
+        /* padding: 0; */
         box-sizing: border-box;
     }
     .widget-messageCalender-wrapper {
