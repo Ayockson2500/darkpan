39b589aa537d58472d2d98f034283ff15e35a6f3
ayockson2500 <ayockson2500@gmail.com>
2022-11-12 07:37:20 +0100

    main navBar updated

---
+++ src/pages/landingPage/navbar/MainNavBar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import StyledMainNavBar from "./StyledMainNavBar.styles";
+import { navElements } from "./navElements";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../../../module/common/buttons/Button";
+
+const MainNavBar = ({isSignIn, setIsSignIn}) => {
+    console.log(isSignIn);
+    const navigate = useNavigate()
+
+    // const handleLogin = () => {
+
+    // }
+    if (!isSignIn) {
+        setIsSignIn(false)
+    } else {
+        setIsSignIn(true)
+        navigate("/dashboard")
+    }
+  return (
+    <StyledMainNavBar >
+        <div>
+            <h1 className="logo">Logo</h1>
+        </div>
+      <div className="d-flex">
+        {navElements.map((item, idx) => (
+          <p className="me-4" key={idx}>
+            <Link>{item.title}</Link>
+          </p>
+        ))}
+        <Button className={"me-3 signIn-Btn"}><Link to={"/signin"}>Log In</Link></Button>
+        <Button className={"signUp-btn"}><Link to={"/signup"}>Sign Up</Link></Button>
+      </div>
+    </StyledMainNavBar>
+  );
+};
+
+export default MainNavBar;
---
+++ src/pages/landingPage/navbar/StyledMainNavBar.styles.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const StyledMainNavBar = styled.div`
+    background: ${props => props.theme.background.mainHompageDarkBg};
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px;
+    a {
+        text-decoration: none;
+        color: ${props => props.theme.color.mainNavyColor};
+        font-size: ${props => props.theme.fontsize.fontsize20};
+        font-weight: ${props => props.theme.fontWight.fontWight70};
+    }
+    .logo {
+        color: ${props => props.theme.color.mainRedColor};
+    }
+    Button {
+        padding: 2px 15px;
+    }
+    Button a {
+        font-size: 12px;
+    }
+    .signUp-btn {
+        background: #EDEDED;
+        border: 1px solid red;
+    }
+`
+
+export default StyledMainNavBar;
