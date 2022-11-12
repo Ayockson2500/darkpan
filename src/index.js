a2a711c11b7bdfffb7ee681c051db32988eee34d
ayockson2500 <ayockson2500@gmail.com>
2022-11-12 07:33:31 +0100

    added configureStore

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import StyledThemeProvider from "./theme";
+import { Provider } from "react-redux";
+import { configureStore, compose } from "@reduxjs/toolkit";
+import reducers from "./redux/reducers";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
+const composeEnhencer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = configureStore(
+  {reducer: reducers},
+  composeEnhencer()
+);
 
 root.render(
+  <Provider store={store}>
     <StyledThemeProvider>
-     <App />
+      <App />
     </StyledThemeProvider>
-)
+  </Provider>
+);
