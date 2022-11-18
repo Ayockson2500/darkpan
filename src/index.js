c4abd80dc71f9606c4879064391f13e9a19090b6
ayockson2500 <ayockson2500@gmail.com>
2022-11-18 08:37:10 +0100

    import store file

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import StyledThemeProvider from "./theme";
 import { Provider } from "react-redux";
-import { configureStore, compose } from "@reduxjs/toolkit";
-import reducers from "./redux/reducers";
+import { store } from "./redux/store";
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-const composeEnhencer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = configureStore(
-  {reducer: reducers},
-  composeEnhencer()
-);
+
 
 root.render(
   <Provider store={store}>
