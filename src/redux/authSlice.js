9b9501ecbd533960ee43e7351cb55e6171434839
ayockson2500 <ayockson2500@gmail.com>
2022-11-18 08:38:34 +0100

    created a slice file for my redux state

---
+++ src/redux/authSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const  initialState = {
+    isLoggin: null,
+    isLoading: false
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducer: {}
+})
+
+console.log(authSlice);
+
+export default authSlice.reducer
